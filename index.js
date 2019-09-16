3c7548c38fd8e25af0f4a945db38296ae5827ba3
Nikoloz Mchedlishvili <nikolozmchedlishvili@Nikolozs-MacBook-Pro.local>
2019-09-16 14:51:52 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require ('body-parser');
+const nodemailer = require('nodemailer');
+const app =express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('/api/form', (req, res)=> {
+
+const transporter = nodemailer.createTransport({
+    host: 'mail.niko.ge',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'niko@niko.ge',
+        pass: 'gaebi1969'
+    }
+    });
+
+let mailOptions = {
+
+    from: 'niko@niko.ge',
+    to: 'mchedlish@yahoo.com',
+    name:req.body.name,
+    subject: 'New Message',
+    text: req.body.message
+   
+}
+
+transporter.sendMail(mailOptions);
+
+
+
+
+   
+    console.log(req.body)
+
+
+
+});
+
+const PORT=process.env.PORT||3001;
+app.listen(PORT, ()=>{
+console.log(`Server listening to ${PORT}`)
+
+
+})
