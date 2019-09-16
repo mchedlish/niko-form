8a95addbdffa77500682e8c5b7983c717177af2b
Nikoloz Mchedlishvili <nikolozmchedlishvili@Nikolozs-MacBook-Pro.local>
2019-09-16 15:01:00 +0400

    port changed

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ transporter.sendMail(mailOptions);
 
 });
 
-const PORT=process.env.PORT||3001;
+const PORT=process.env.PORT||5000;
 app.listen(PORT, ()=>{
 console.log(`Server listening to ${PORT}`)
 
